21e047cb357e4a224228079117143e96056a02ba
jmbento <47256444+jmbento@users.noreply.github.com>
2026-03-11 10:08:45 -0300

    feat: add epson module with printer class

---
+++ epson/__init__.py
@@ -0,0 +1,13 @@
+"""
+Epson Ink Pad Resetter
+Free and open-source tool to reset waste ink counters on Epson printers
+
+Version: 1.0.0
+License: GPL v3
+Author: jmbento
+"""
+
+from .printer import EpsonPrinter
+
+__version__ = "1.0.0"
+__all__ = ["EpsonPrinter"]
